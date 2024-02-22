d2a08c48a86cc8ff5a2c9531704c76fb1630ba9c
William Atuesta <w.atuesta@gmail.com>
2024-02-21 20:08:26 -0500

    Se termina logica para encriptar y desencriptar de la app y se agregan los metodos a los botones de encriptar y desencriptar

--- js/app.js
+++ js/app.js
@@ -1,6 +1,44 @@
-let mensaje = document.querySelector("#texto-encriptado");
+let mensaje = document.querySelector('#texto-sin-encriptar');
+let mensaje_encriptado = document.querySelector('#texto-encriptado');
+
+function limpiarCaja() {
+    document.querySelector('#texto-sin-encriptar').value = '';
+}
+
+let matriz_clave = [
+    ["a", "#/("],
+    ["e", "?)|"],
+    ["i", "!¿¡"],
+    ["o", "@=&"],
+    ["u", "*%$"],
+];
+
+function encriptar(frase) {
+    for (let i = 0; i < matriz_clave.length; i++) {
+        if (frase.includes(matriz_clave[i][0])) {
+            frase = frase.replaceAll(matriz_clave[i][0], matriz_clave[i][1]);
+        }
+    }
+    return frase;
+}
 
 function btnEncriptar(){
     let texto = mensaje.value;
-    console.log(texto);
+    mensaje_encriptado.innerHTML = encriptar(texto.toLowerCase());
+    limpiarCaja();
+}
+
+function desencriptar(frase) {
+    for (let i = 0; i < matriz_clave.length; i++) {
+        if (frase.includes(matriz_clave[i][1])) {
+            frase = frase.replaceAll(matriz_clave[i][1], matriz_clave[i][0]);
+        }
+    }
+    return frase;
+}
+
+function btnDesencriptar(){
+    let texto = mensaje.value;
+    mensaje_encriptado.innerHTML = desencriptar(texto.toLowerCase());
+    limpiarCaja();
 }
